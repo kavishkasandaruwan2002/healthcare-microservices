3c555c51d2801947d4e5d3c8576d1bcc5612ed9c
ImeshaDissa <dissanayakalis60@gmail.com>
2026-04-24 00:51:30 +0530

    fix: check

--- services/payment-service/src/main/java/com/healthcare/payment/PaymentApplication.java
+++ services/payment-service/src/main/java/com/healthcare/payment/PaymentApplication.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaClient// error check
 @RestController
 public class PaymentApplication {
 
